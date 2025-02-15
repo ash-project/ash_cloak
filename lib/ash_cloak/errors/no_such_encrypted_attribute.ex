8b4c00f6355070f6ee305e9dff7d1566fb7f903e
Terry Lee <terry.lee.m@gmail.com>
2025-02-15 22:54:56 +0000

    Make AshCloak.encrypt_and_set raise a specific error. (#72)
    * Make AshCloak.encrypt_and_set return a specific error.
    
    * Update to have AshCloak.encrypt_and_set return new specific Error for non-encryptable attribute

---
+++ lib/ash_cloak/errors/no_such_encrypted_attribute.ex
@@ -0,0 +1,19 @@
+defmodule AshCloak.Errors.NoSuchEncryptedAttribute do
+  @moduledoc """
+  An error raised when attempting to decrypt an attribute that is not encrypted.
+  """
+
+  use Splode.Error, fields: [:key, :resource], class: :invalid
+  
+  def message(error) do
+    """
+    Attempted to encrypt and set attribute#{for_key(error)}#{for_resource(error)}, but it is not configured for encryption.}
+    """
+  end
+
+  defp for_key(%{key: key}) when not is_nil(key), do: " for #{key}"
+  defp for_key(_), do: ""
+
+  defp for_resource(%{resource: resource}) when not is_nil(resource), do: " for #{resource}"
+  defp for_resource(_), do: ""
+end
