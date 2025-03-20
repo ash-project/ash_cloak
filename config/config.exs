9ebf4b1c081eec9106cb954a6f6c0d4ec6c4be76
Zach Daniel <zach@zachdaniel.dev>
2025-03-20 18:26:43 -0400

    chore: use config for tests to pass

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,7 @@ import Config
 config :ash, :disable_async?, true
 config :ash, :validate_domain_resource_inclusion?, false
 config :ash, :validate_domain_config_inclusion?, false
+config :ash, :keep_read_action_loads_when_loading?, false
 
 if Mix.env() == :dev do
   config :git_ops,
