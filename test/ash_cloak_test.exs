c0b31095d89b98ed1ee6d0ef8d20c9d026ab7872
Leon Qadirie <39130739+leonqadirie@users.noreply.github.com>
2026-02-02 07:08:04 +0100

    chore: SPDX contributor links (#135)

--- test/ash_cloak_test.exs
+++ test/ash_cloak_test.exs
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024 ash_cloak contributors <https://github.com/ash-project/ash_cloak/graphs.contributors>
+# SPDX-FileCopyrightText: 2024 ash_cloak contributors <https://github.com/ash-project/ash_cloak/graphs/contributors>
 #
 # SPDX-License-Identifier: MIT
 
